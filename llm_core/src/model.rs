c014e673ce72ac29669ae25de4bc36d2876a25a9
bikaldev <devkotabikal777@gmail.com>
2024-09-03 20:43:22 +0545

    Removed the binary crate

--- llm_core/src/model.rs
+++ llm_core/src/model.rs
@@ -1,5 +1,5 @@
 use crate::message;
 
 pub trait Model {
-    fn invoke(&mut self, message:  message::Message) -> Result<message::Message, Box<dyn std::error::Error>>; 
+    fn invoke(&mut self, message:  message::Message) -> Result<message::Message, Box<dyn std::error::Error>>;
 }
